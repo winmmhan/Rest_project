d1a1985ccc43d665dd74cf5b926b9772f9463cb0
winmmhan <win.han@outlook.com>
2018-06-24 22:14:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,57 +2,43 @@
 include('includes/newconfig.php');
 include('includes/database.php');
 
-$database = new Database();
-
-//if($database) {echo 'true';}
-
+global $database;
 
 $startTime = time();
 $endTime = $startTime -(5 * 60);
-
 echo '<br/>' . $endTime;
 
-//$edTime = new DateTime("@$endTime");
-//echo '<br/>' . $edTime->format('Y-m-d H:i:s') . '<br/>';
-
+//Create Array for each timestamp 5 minutes backwards
 $timeArray = array();
 while($startTime >= $endTime ){
-    
-//    echo '<br/>' .$startTime . ' ' ;
-//     $stTime = new DateTime("@$startTime");   
-//     echo  ' '.  $stTime->format('Y-m-d H:i:s') . '<br/>';
     
 	    $cpuload =round(getRandFloat(0,100));
 	    $concurrency=rand(0,500000);
 
 	    $timeArray[]  = array(
 		"timestamp"=>$startTime,
-		"cpuLoad"=>$cpuload,
+		"cpuload"=>$cpuload,
 		"concurrency"=>$concurrency,       
 	    ); 
 	
 	 $startTime = $startTime - 1;
-}       
+   }       
 
-insertData();
 
-function insertData(){
-    	foreach($timeArray as $arr){
+foreach($timeArray as $arr){
+         
+	$timestamp = $arr['timestamp'];
+	$cpuload = $arr['cpuload'];
+	$con = $arr['concurrency'];
 
-         	//   echo $arr['timestamp'] .' ' . $arr['cpuload']. ' '. $arr['concurrency'] ."<br/><br/>"; 
-		$timestamp = $arr['timestamp'];
-		$cpuload = $arr['cpuLoad'];
-		$con = $arr['concurrency'];
-
-		$sql = "INSERT INTO datapoints (id, timestamp, cpu_load, concurrency) VALUES "; 
-        	$sql .= "('', '".$timestamp."', '".$cpuload."', '".$con."')"; 
+	$sql = "INSERT INTO datapoints (id, timestamp, cpu_load, concurrency) VALUES "; 
+        $sql .= "('', '".$timestamp."', '".$cpuload."', '".$con."')"; 
 		
-		$database->query($sql);
-    }         
-}
+	$database->query($sql);
+ }         
 
-//var_dump($myArray);
 
+// Generate Random Float number
 function getRandFloat($Min, $Max){
 	//validate input
 	if ($Min>$Max) { $min=$Max; $max=$Min; }
